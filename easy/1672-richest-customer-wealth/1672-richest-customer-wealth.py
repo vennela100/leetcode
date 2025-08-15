0ffd168c71c6bf6269ce789d47935a0574be70ea
vennela100 <alamurvennela2007@gmail.com>
2025-08-15 22:00:34 +0530

    Add solution for 1672. Richest Customer Wealth

---
+++ easy/1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,12 @@
+            ans.append(sum(i))
+        for i in accounts:
+        ans=[]
+            
+            
+        """
+        :rtype: int
+        :type accounts: List[List[int]]
+        """
+    def maximumWealth(self, accounts):
+class Solution(object):
+        return max(ans)
