8c0931d832714629b56ac572db2823086dd6fadd
vennela100 <alamurvennela2007@gmail.com>
2025-08-20 23:04:51 +0530

    Update 1365-how-many-numbers-are-smaller-than-the-current-number.py

--- easy/1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ easy/1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,10 +1,11 @@
         """
         result=[]
         for i in nums:
+              count=0
             for j in range(len(nums)):
                 if i>nums[j]:
                     count+=1
             result.append(count)
         return result
         
-            count=0
+            
