718671da56d9104344da3f19b6c0da75e52cadf1
vennela100 <alamurvennela2007@gmail.com>
2025-08-20 22:54:03 +0530

    Add solution for 1512. Number of Good Pairs

---
+++ easy/1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -0,0 +1,10 @@
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j]:
+        for i in range(len(nums)):
+        count=0
+        """
+        :rtype: int
+        :type nums: List[int]
+                    count+=1
+        return count
+        
