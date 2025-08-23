2ff74912bda0049b41a790b5d59ca34a001b4035
vennela100 <alamurvennela2007@gmail.com>
2025-08-23 18:42:36 +0530

    Add solution for 1832. Check if the Sentence Is Pangram

---
+++ easy/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -0,0 +1,7 @@
+        :type sentence: str
+        :rtype: bool
+        """
+        """
+    def checkIfPangram(self, sentence):
+class Solution(object):
+        return set(string.ascii_lowercase)<=set(sentence.lower())
