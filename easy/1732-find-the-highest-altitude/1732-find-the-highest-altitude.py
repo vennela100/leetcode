1dfab1b2dbd144dd055d23b236d53f2e413b2971
vennela100 <alamurvennela2007@gmail.com>
2025-08-23 22:27:59 +0530

    Add solution for 1732. Find the Highest Altitude

---
+++ easy/1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,10 @@
+        """
+        :type gain: List[int]
+        :rtype: int
+        """
+        start=0
+        for i in gain:
+        result=[0]
+            result.append(start)
+        return max(result)
+            start=start+i
