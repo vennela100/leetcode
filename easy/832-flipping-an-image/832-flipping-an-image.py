887c80cce1c2befd88dc11593a614f4b939b8615
vennela100 <alamurvennela2007@gmail.com>
2025-08-25 23:14:25 +0530

    Add solution for 832. Flipping an Image

---
+++ easy/832-flipping-an-image/832-flipping-an-image.py
@@ -0,0 +1,9 @@
+                if j==0:
+            for j in i:
+            new=[]
+            i.reverse()
+        for i in image:
+        result=[]
+        """
+        :rtype: List[List[int]]
+        :type image: List[List[int]]
