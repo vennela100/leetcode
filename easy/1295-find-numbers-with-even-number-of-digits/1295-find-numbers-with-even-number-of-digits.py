078fc865025941c237a5e64bb144b8569054b99b
vennela100 <alamurvennela2007@gmail.com>
2025-08-28 23:11:56 +0530

    Add solution for 1295. Find Numbers with Even Number of Digits

---
+++ easy/1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,8 @@
+            for j in str(i):
+            count=0
+                count+=1
+        for i in nums:
+            if count%2==0:
+                even+=1
+        return even
+        
