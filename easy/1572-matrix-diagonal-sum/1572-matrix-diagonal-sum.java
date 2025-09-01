10ac3be6b8670a542c260913f0e176c99e6f54c5
vennela100 <alamurvennela2007@gmail.com>
2025-09-01 23:43:32 +0530

    Add solution for 1572. Matrix Diagonal Sum

---
+++ easy/1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -0,0 +1,9 @@
+            if i==n-i-1:
+        for i in range(n):
+        total=0
+        n=len(mat)
+        """
+                total+=mat[i][i]
+            else:
+                total+=mat[i][i]+mat[i][n-i-1]
+        
