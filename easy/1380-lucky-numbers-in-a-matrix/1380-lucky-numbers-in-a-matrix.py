cfc011e5a949fd75efc82d03446b8defcb0ac910
vennela100 <alamurvennela2007@gmail.com>
2025-09-06 23:25:30 +0530

    Add solution for 1380. Lucky Numbers in a Matrix

---
+++ easy/1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -0,0 +1,10 @@
+                sum.append(matrix[j][i])
+                a=max(sum)
+            for j in range(len(matrix)):
+        for i in range(len(matrix[0])):
+        
+        
+            result.append(min(h))
+            sum=[]
+            result.append(a)
+        for y in range(len(result)):
